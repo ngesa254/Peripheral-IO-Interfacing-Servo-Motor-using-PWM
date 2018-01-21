9abe819f337ff3cbf3ee0a7eb6c8dd35421ccb98
ngesa marvin <marvinngesa@gmail.com>
2018-01-21 08:23:16 +0300

    add pwmservo driver

--- app/src/main/java/com/droidmarvin/peripheraliointerfacingservomotorusingpwm/MainActivity.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingservomotorusingpwm/MainActivity.java
@@ -4,17 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.things.contrib.driver.pwmservo.Servo;
 import com.google.android.things.pio.PeripheralManagerService;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
 
+    private static final String PWM_BUS = "PWM1";
+
+    private Servo mServo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         PeripheralManagerService service = new PeripheralManagerService();
-        Log.d(TAG,"Available PWM:" +service.getPwmList());
+//        Log.d(TAG,"Available PWM:" +service.getPwmList());
     }
 }
