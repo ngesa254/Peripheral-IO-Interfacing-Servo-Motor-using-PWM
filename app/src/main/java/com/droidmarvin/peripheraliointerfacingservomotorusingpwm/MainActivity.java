584dcead28305cc40d7000fae2d3f4ca70308cd5
ngesa marvin <marvinngesa@gmail.com>
2018-01-21 07:57:30 +0300

    initial things commit

---
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacingservomotorusingpwm/MainActivity.java
@@ -0,0 +1,20 @@
+package com.droidmarvin.peripheraliointerfacingservomotorusingpwm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.PeripheralManagerService;
+
+public class MainActivity extends Activity {
+
+//    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        PeripheralManagerService service = new PeripheralManagerService();
+//        Log.d(TAG,"Available PWM:" +service.getPwmList());
+    }
+}
